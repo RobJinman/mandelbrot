dcfac0a63af4daa460ce7e9a14da6edd0c9b0cd6
Rob Jinman <jinmanr@gmail.com>
2019-02-19 20:00:17 +0000

    Initial commit

---
+++ src/config_in.hpp
@@ -0,0 +1,3 @@
+#define Mandelbrot_VERSION_MAJOR @Mandelbrot_VERSION_MAJOR@
+#define Mandelbrot_VERSION_MINOR @Mandelbrot_VERSION_MINOR@
+
---
+++ src/mandelbrot.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "config.hpp"
+
+
+int main() {
+  std::cout << "Mandelbrot " << Mandelbrot_VERSION_MAJOR << "." << Mandelbrot_VERSION_MINOR << "\n";
+
+  return 0;
+}
+
