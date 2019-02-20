fa90e4a8921a2bbea9a64c583c743eecd6e83e9e
Rob Jinman <jinmanr@gmail.com>
2019-02-20 22:35:49 +0000

    Added GLFW dependency and linked static libs

--- cmake/external_glfw.cmake
+++ cmake/external_glfw.cmake
@@ -1,7 +1,6 @@
 include(ExternalProject)
 
-set(
-  GLEW_CMAKE_ARGS
+set(GLFW_CMAKE_ARGS
   -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
   -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR> 
   -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
@@ -10,12 +9,11 @@ set(
   -DBUILD_SHARED_LIBS=FALSE
 )
 
-ExternalProject_Add(
-  libglew
-  URL https://github.com/nigels-com/glew/releases/download/glew-2.1.0/glew-2.1.0.tgz
-  PREFIX glew_src
-  CMAKE_ARGS ${GLEW_CMAKE_ARGS}
-  SOURCE_SUBDIR build/cmake
+ExternalProject_Add(libglfw
+  PREFIX glfw_src
+  GIT_REPOSITORY https://github.com/glfw/glfw.git
+  GIT_TAG 3.2.1
+  CMAKE_ARGS ${GLFW_CMAKE_ARGS}
   INSTALL_DIR ${PROJECT_SOURCE_DIR}/../vendor
   LOG_CONFIGURE 1
   LOG_BUILD 1
