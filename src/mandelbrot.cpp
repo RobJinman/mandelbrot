89db5a8277dcf3887dda3e20067dc72c6a73bd13
Rob Jinman <jinmanr@gmail.com>
2019-02-21 19:58:40 +0000

    Draws red triangle with basic shaders

--- src/mandelbrot.cpp
+++ src/mandelbrot.cpp
@@ -1,10 +1,69 @@
+#include <string>
 #include <iostream>
+#include <fstream>
+#include <vector>
 #include <cstdio>
 #include <cstdlib>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include "config.hpp"
 
+using std::string;
+using std::vector;
+
+GLuint loadShader(const string& srcPath, GLuint type) {
+  GLuint shaderId = glCreateShader(type);
+
+  std::ifstream fin(srcPath);
+  string shaderSrc{std::istreambuf_iterator<char>(fin),
+                   std::istreambuf_iterator<char>()};
+
+  GLint result = GL_FALSE;
+  int infoLogLen = 0;
+
+  const char* srcPtr = shaderSrc.c_str();
+  glShaderSource(shaderId, 1, &srcPtr, NULL);
+  glCompileShader(shaderId);
+
+  glGetShaderiv(shaderId, GL_COMPILE_STATUS, &result);
+  glGetShaderiv(shaderId, GL_INFO_LOG_LENGTH, &infoLogLen);
+  if (infoLogLen > 0) {
+    vector<char> errMsg(infoLogLen + 1);
+    glGetShaderInfoLog(shaderId, infoLogLen, NULL, errMsg.data());
+    std::cerr << errMsg.data() << std::endl;
+  }
+
+  return shaderId;
+}
+
+GLuint loadShaders(const string& vertShaderPath, const string& fragShaderPath) {
+  GLuint vertShader = loadShader(vertShaderPath, GL_VERTEX_SHADER);
+  GLuint fragShader = loadShader(fragShaderPath, GL_FRAGMENT_SHADER);
+
+  GLuint program = glCreateProgram();
+  glAttachShader(program, vertShader);
+  glAttachShader(program, fragShader);
+  glLinkProgram(program);
+
+  GLint result = GL_FALSE;
+  int infoLogLen = 0;
+
+  glGetProgramiv(program, GL_LINK_STATUS, &result);
+  glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infoLogLen);
+  if (infoLogLen > 0) {
+    vector<char> errMsg(infoLogLen + 1);
+    glGetProgramInfoLog(program, infoLogLen, NULL, errMsg.data());
+    std::cerr << errMsg.data() << std::endl;
+  }
+
+  glDetachShader(program, vertShader);
+  glDetachShader(program, fragShader);
+
+  glDeleteShader(vertShader);
+  glDeleteShader(fragShader);
+
+  return program;
+}
 
 int main() {
   std::cout << "Mandelbrot " << Mandelbrot_VERSION_MAJOR << "."
@@ -41,8 +100,35 @@ int main() {
 
   glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
+  GLuint vertexArray;
+  glGenVertexArrays(1, &vertexArray);
+  glBindVertexArray(vertexArray);
+
+  static const GLfloat vertexBufferData[] = {
+    -1.0f, -1.0f, 0.0f,
+    1.0f, -1.0f, 0.0f,
+    0.0f, 1.0f, 0.0f
+  };
+
+  GLuint vertexBuffer;
+  glGenBuffers(1, &vertexBuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertexBufferData), vertexBufferData,
+               GL_STATIC_DRAW);
+
+  GLuint program = loadShaders("data/vert_shader.glsl",
+                               "data/frag_shader.glsl");
+
   do {
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glUseProgram(program);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDisableVertexAttribArray(0);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
