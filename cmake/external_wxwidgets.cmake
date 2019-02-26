5b7dc5503261166b1789a524c14d66744f373087
Rob Jinman <jinmanr@gmail.com>
2019-02-26 08:22:45 +0000

    Additional tweaks to build code needed to get working on windows 10 VM

--- cmake/external_wxwidgets.cmake
+++ cmake/external_wxwidgets.cmake
@@ -1,8 +1,9 @@
+cmake_minimum_required(VERSION 3.10)
 include(ExternalProject)
+include(common)
 
 set(WX_CMAKE_ARGS
   -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
-  -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR> 
   -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
   -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
   -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES}
@@ -13,7 +14,7 @@ ExternalProject_Add(libwx
   PREFIX wx_src
   URL https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.2/wxWidgets-3.1.2.tar.bz2
   CMAKE_ARGS ${WX_CMAKE_ARGS}
-  INSTALL_DIR ${PROJECT_SOURCE_DIR}/../vendor
+  INSTALL_DIR ${PROJECT_SOURCE_DIR}/../vendor/${PLATFORM_NAME}
   LOG_CONFIGURE 1
   LOG_BUILD 1
   LOG_INSTALL 1
