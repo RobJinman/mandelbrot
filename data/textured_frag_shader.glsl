922d439f959419de71625df24649f5ae8cc5f0be
Rob Jinman <jinmanr@gmail.com>
2019-03-03 14:03:46 +0000

    Fix textured frag shader on OS X

--- data/textured_frag_shader.glsl
+++ data/textured_frag_shader.glsl
@@ -1,11 +1,11 @@
 #version 330 core
 
-uniform sampler2D texture;
+uniform sampler2D texSampler;
 
 in vec2 vv_tex;
 
 layout(location = 0) out vec3 out_colour;
 
 void main() {
-  out_colour = texture2D(texture, vv_tex).xyz;
+  out_colour = texture(texSampler, vv_tex).xyz;
 }
